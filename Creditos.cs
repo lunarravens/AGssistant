5d3687c5f4ca20dab8d7af0eeffdd44fbac89b6c
João Vitor <105260702+lunarravens@users.noreply.github.com>
2024-11-24 00:24:45 -0300

    Update 24/11 - Little QOL tweaks

--- Creditos.cs
+++ Creditos.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Reflection.Emit;
@@ -27,7 +28,7 @@ namespace AGssistant_V2
 
             label2.BackColor = ColorTranslator.FromHtml("#1c5560");
 
-            this.CenterToScreen(); 
+            this.CenterToScreen();
         }
 
         private void label7_Click(object sender, EventArgs e)
@@ -45,6 +46,30 @@ namespace AGssistant_V2
             telaInicial telaInicial = new telaInicial();
             telaInicial.Show();
             this.Hide();
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+            string url = "https://www.linkedin.com/in/isabela-boni-53500a277/";
+            Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+            string url = "https://www.linkedin.com/in/jean-yoshida-90285b325/";
+            Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+            string url = "https://www.linkedin.com/in/joão-vitor-gozzo-bruschi-36447522a/";
+            Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+            string url = "https://www.linkedin.com/in/elysiumrev/";
+            Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
         }
     }
 }
